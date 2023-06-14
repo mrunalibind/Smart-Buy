42f707bc7c6e24107b148ad0459b2cc2bee2b451
Himanshuj0311 <himanshu.masai@gmail.com>
2023-06-14 22:34:36 +0530

    day-2 complet

---
+++ Frontend/script/signup.js
@@ -0,0 +1,27 @@
+const onSignup= ()=>{
+
+    const payload={
+    
+    name:document.getElementById("name").value,
+     email:document.getElementById("Email").value,
+      pass:document.getElementById("Password").value
+    
+    }
+    
+    fetch("http://localhost:8000/user/reg", {
+    
+    method: "POST",
+    
+    headers: {
+    
+    "Content-type":"application/json"
+    
+    },
+    
+    body: JSON.stringify(payload)
+    
+    }).then(res=>res.json())
+     .then(res=>console.log(res))
+    .catch(err=>console.log(err))
+    
+    }
