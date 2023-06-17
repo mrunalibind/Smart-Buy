844ba4e28bddbb731670f7546200823bb77b18e9
Priyanka Jha <krishukumar7827@gmail.com>
2023-06-17 10:41:06 +0530

    API

--- Backend_API/db.js
+++
@@ -1,7 +0,0 @@
-let mongoose = require("mongoose")
-require('dotenv').config()
-
-let connection = mongoose.connect(process.env.mongodb)
-
-
-module.exports = {connection}
--- Backend_API/redis.db.js
+++
@@ -1,7 +0,0 @@
-let Redis = require('ioredis')
-
-let redis = new Redis()
-
-console.log("redis")
-
-module.exports = {redis}
